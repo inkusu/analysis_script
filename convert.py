e6718dd3ea1500e1b70a57f4e4c0d8c5e2661b18
inkusu <lnkusuin@gmail.com>
2019-06-04 10:47:03 +0900

    警告調整

--- convert.py
+++ convert.py
@@ -8,19 +8,18 @@ if __name__ == '__main__':
     convert = {}
     for file_path in sorted(glob.glob('./log/2019*.json'), reverse=True):
         with open(file_path) as f:
-        	data = json.load(f)
-        	appneds = [
-        	    tweet for tweet in data['statuses'] 
-        	    if not tweet['text'].startswith('RT ') 
-        	    and not 'bot' in tweet['user']['screen_name'] 
-        	    and tweet['id_str'] not in convert
-        	]
-        	
-        	convert.update({ item['id_str']: item for item in appneds})
-        	
+            data = json.load(f)
+            appends = [
+                tweet for tweet in data['statuses']
+                if not tweet['text'].startswith('RT ')
+                and 'bot' not in tweet['user']['screen_name']
+                and tweet['id_str'] not in convert
+            ]
+
+            convert.update({item['id_str']: item for item in appends})
+
     save_path = './log/convert.json'
     if os.path.isfile(save_path):
-        os.remove(save_path)    
+        os.remove(save_path)
     with open(save_path, 'a') as f:
         json.dump(list(convert.values()), f, indent=4, ensure_ascii=False, )
-        	
